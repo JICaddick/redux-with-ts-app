053ecce259ea7b9601938e52b7570d6d101bd224
JICaddick <james_caddick16@hotmail.com>
2023-02-18 22:37:15 -0500

    First few pages set up- TS and general errors to fix

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,16 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import postsReducer from '../features/posts/postsSlice'
 
 export const store = configureStore({
   reducer: {
-    // Add your reducers here
+    posts: postsReducer,
   },
 });
 
-export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
   RootState,
